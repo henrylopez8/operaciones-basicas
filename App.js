247463cb8e909710d167aacb809c562ecb523532
henrylopez8 <henrylopez1998.hfl@gmail.com>
2021-08-23 14:48:03 -0600

    version 1

--- App.js
+++ App.js
@@ -1,12 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+    const[text1,settext1]=useState('');
+  const[text2,settext2]=useState('');
+
+  const[resultado,setresultado]=useState('');
+
+  function Sumar(var1,var2){
+    var a = parseFloat(var1);
+    var b= parseFloat(var2);
+    var c= a+b;
+    var resul="resultado:"+a+" + "+b+"= "+c;
+    setresultado(resul);
+  }
+    function Restar(var1,var2){
+    var a = parseFloat(var1);
+    var b= parseFloat(var2);
+    var c= a-b;
+    var resul="resultado:"+a+" - "+b+"= "+c;
+    setresultado(resul);
+  }
+  function Multiplicar(var1,var2){
+    var a = parseFloat(var1);
+    var b= parseFloat(var2);
+    var c= a*b;
+    var resul="resultado:"+a+" x "+b+"= "+c;
+    setresultado(resul);
+  }
+  function Dividir(var1,var2){
+    var a = parseFloat(var1);
+    var b= parseFloat(var2);
+    var resul
+    if(b!=0){
+         var c= a/b;
+      resul="resultado:"+a+" ÷ "+b+"= "+c;
+    }
+    else{
+      resul="no se puede dividir entre 0"
+    }
+   
+    setresultado(resul);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+ <View style={styles.container}>
+      <Text style={{padding:15, fontSize:24, backgroundColor:'orange',color:'white', textAlign:'center'}}>
+        Suma de dos numeros
+      </Text>
+      <TextInput style={{padding:5,
+      height:40,
+      borderColor:'blue',
+      borderWidth:2,
+      marginTop:10
+      }}
+      placeholder='Valor 1'
+      keyboardType='numeric'
+      onChangeText={(text1)=>settext1(text1)}
+      />
+          <TextInput style={{padding:5,
+      height:40,
+      borderColor:'blue',
+      borderWidth:2,
+      marginTop:10
+      }}
+      placeholder='Valor 2'
+      keyboardType='numeric'
+      onChangeText={(text2)=>settext2(text2)}
+      />
+<TouchableOpacity
+style={{height:60, backgroundColor:'blue', marginTop:10}}
+onPress={()=>{Sumar(text1,text2)}}
+>
+<Text style={{color:'white',fontSize:24,textAlign:'center'}}>
+  Sumar
+</Text>
+</TouchableOpacity>
+<TouchableOpacity
+style={{height:60, backgroundColor:'blue', marginTop:10}}
+onPress={()=>{Restar(text1,text2)}}
+>
+<Text style={{color:'white',fontSize:24,textAlign:'center'}}>
+  Restar
+</Text>
+</TouchableOpacity>
+<TouchableOpacity
+style={{height:60, backgroundColor:'blue', marginTop:10}}
+onPress={()=>{Multiplicar(text1,text2)}}
+>
+<Text style={{color:'white',fontSize:24,textAlign:'center'}}>
+  Multiplicar
+</Text>
+</TouchableOpacity>
+<TouchableOpacity
+style={{height:60, backgroundColor:'blue', marginTop:10}}
+onPress={()=>{Dividir(text1,text2)}}
+>
+<Text style={{color:'white',fontSize:24,textAlign:'center'}}>
+  Dividir
+</Text>
+</TouchableOpacity>
+<Text style={{fontSize:32, textAlign:"center",marginTop:20}}>{resultado}</Text>
     </View>
   );
 }
@@ -14,8 +109,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
